8a22eb63dd8e45a8e2151523fec337b56967c44b
Himanshu Verma <himanshuverma544@gmail.com>
2025-07-01 12:23:55 +0530

    upd: added safety logic for login

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -6,6 +6,10 @@ import { signAccessToken, signRefreshToken } from '@/pages/api/auth/utils/index'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
   const { email, password, useHttpCookie } = req.body;
 
   // Dummy auth check
