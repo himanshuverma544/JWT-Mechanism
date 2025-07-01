4b98c80383f0e1a6812a3eb92242e0de81c02fdc
Himanshu Verma <himanshuverma544@gmail.com>
2025-07-01 13:46:06 +0530

    upd: debugged

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -6,7 +6,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <title>JWT Mechanism</title>
+          <meta name="description" content="JWT Mechanism"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
